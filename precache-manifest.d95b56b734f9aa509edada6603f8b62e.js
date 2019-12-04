a23f30ecb59710e7595d2558258678c58c9e9bd1
halfmoonpdh <enqn0903@naver.com>
2019-12-05 02:00:11 +0900

    Updates

---
+++ precache-manifest.d95b56b734f9aa509edada6603f8b62e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7cfda9957dce7766d342d1cdafbdf60",
+    "url": "/duflix/index.html"
+  },
+  {
+    "revision": "7f0a7bd104fd0644cd16",
+    "url": "/duflix/static/js/2.c643e657.chunk.js"
+  },
+  {
+    "revision": "948c6d6677e06aaae510",
+    "url": "/duflix/static/js/main.3ac7bfcb.chunk.js"
+  },
+  {
+    "revision": "f5e5a25add1e0dabd700",
+    "url": "/duflix/static/js/runtime-main.0e417a7b.js"
+  }
+]);
