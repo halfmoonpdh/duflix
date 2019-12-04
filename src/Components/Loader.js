83eaed898e83d3458fa9380728b45fc6f5780f0d
halfmoonpdh <enqn0903@naver.com>
2019-12-05 01:58:34 +0900

    movie app

---
+++ src/Components/Loader.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  height: 100vh;
+  width: 100wh;
+  display: flex;
+  justify-content: center;
+  font-size: 22px;
+`;
+
+export default () => <Container>Loading...</Container>;
